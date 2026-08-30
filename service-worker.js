385a61ad5b839c0d51090ba0e58361c38506d0ea
Kiieza <49732485+Kiieza@users.noreply.github.com>
2026-08-30 19:56:07 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='meu-fluxo-v4';
+const CACHE='meu-fluxo-v5';
 const ASSETS=['./','./index.html','./styles.css','./styles-v2.css','./app-v2.js','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('meu-fluxo-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
